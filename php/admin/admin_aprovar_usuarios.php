c64b049f3067a927f4a3696e2b720c7591db507d
Alex12Rodrigues <alex.rodoliveira2@gmail.com>
2026-05-13 18:50:01 -0300

    Corrigir fluxo de aprovacao de usuarios no banco simples
    - Fazer query compativel com banco simples e robusto
    - Remover dependencias de colunas que nao existem no banco simples (IDF_FILIAL_ID, IDF_CRIADO_EM)
    - Atualizar busca de usuarios pendentes para usar IDF_FILIAL em vez de join
    - Novos usuarios agora aparecem na tela de aprovacao do admin

--- php/admin/admin_aprovar_usuarios.php
+++ php/admin/admin_aprovar_usuarios.php
@@ -399,3 +399,4 @@ if ($res_historico && mysqli_num_rows($res_historico) > 0) {
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
+
